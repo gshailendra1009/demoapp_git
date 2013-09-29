6cc8ea158f68ef3238115344c73c94b94210deb0
Feodor Fitsner <feodor@appveyor.com>
2013-09-29 13:00:22 -0700

    Initial check-in

---
+++ DemoApp.Service.Tests/BasicTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DemoApp.Service.Tests
+{
+    [TestFixture]
+    public class BasicTests
+    {
+        public void A_equal_B()
+        {
+            int a = 1;
+            int b = 1;
+            Assert.AreEqual(a, b);
+        }
+    }
+}
---
+++ DemoApp.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DemoApp.Web.Global" Language="C#" %>
